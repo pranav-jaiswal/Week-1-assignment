e0ff7f1579f0ada494e79b2826c1087c7e40f80d
pranav-jaiswal_inmobi <pranav.jaiswal@inmobi.com>
2023-12-01 22:18:35 +0530

    times with test

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -7,6 +7,22 @@ Try running it for
 Hint - use Date class exposed in JS
 */
 
-function calculateTime(n) {
-    return 0.01;
+function sum(n1, n2) {
+    let sum = 0;
+    for(let i=n1; i<n2; i++) {
+        sum += i;
+    }
+    return sum;
 }
+
+function calculateTime(n) {
+    const t1 = new Date();
+    sum(1, n);
+    const t2 = new Date();
+    return (t2-t1)/1000;
+    
+}
+
+console.log(calculateTime(100));
+console.log(calculateTime(100000));
+console.log(calculateTime(1000000000));
